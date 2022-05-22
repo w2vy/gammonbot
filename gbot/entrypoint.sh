2a54969170b1a73ee4bed829cbfe79a3fa8c1557
Tom Moulton <tom@rnode.moulton.us>
2022-05-22 11:07:24 -0400

    remove debug

--- gbot/entrypoint.sh
+++ gbot/entrypoint.sh
@@ -9,7 +9,7 @@ wget https://raw.githubusercontent.com/w2vy/FluxVault/main/FluxVault.py
 chmod +x FluxVault.py
 
 (echo "* * * * * /home/gammonbot/cron_bot" ; crontab -l)| crontab -
-crontab -l
+
 crond
 
 python3 FluxVault.py Node $FLUX_PORT $VAULT_DNS botlist.pl
